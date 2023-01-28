bc53818bbd9791215b2d35475b43c0bd5f81e58a
dwbrillantes <debrillantes@gmail.com>
2023-01-28 15:45:16 +0800

    build: add breakpoints to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './components/**/*.{js,ts,tsx,jsx}',
   ],
   theme: {
+    screens: {
+      xs: '375px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1536',
+    },
     extend: {},
   },
   plugins: [],
