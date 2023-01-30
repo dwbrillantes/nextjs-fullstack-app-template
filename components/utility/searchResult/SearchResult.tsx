d2ffec396edee5724b08dbe8c217ef347f985a5b
dwbrillantes <debrillantes@gmail.com>
2023-01-31 07:36:50 +0800

    feat: add boilerplate search result

--- components/utility/searchResult/SearchResult.tsx
+++ components/utility/searchResult/SearchResult.tsx
@@ -1,11 +1,32 @@
-export interface ISearchResult {
-  sampleTextProp: string;
+import Link from 'next/link';
+export interface ISearchResult extends React.ComponentPropsWithoutRef<'div'> {
+  url: string;
+  title: string;
+  text: string;
 }
 
-const SearchResult: React.FC<ISearchResult> = ({ sampleTextProp }) => {
+const SearchResult: React.FC<ISearchResult> = ({
+  url,
+  title,
+  text,
+  className,
+  ...divProps
+}) => {
   return (
-    <div className="bg-gradient-to-bl from-orange-500 via-blue-800 to-black">
-      {sampleTextProp}
+    <div
+      {...divProps}
+      className={`flex flex-col w-5/6 max-w-screen-md space-y-1 ${className}`}
+    >
+      <Link
+        href={url}
+        className="hover:underline"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p>{url}</p>
+        <p className="text-blue-600 text-xl">{title}</p>
+      </Link>
+      <p>{text}</p>
     </div>
   );
 };
---
+++ pages/results.tsx
@@ -0,0 +1,25 @@
+import PrimaryLayout from '@/components/layouts/primary/PrimaryLayout';
+import SearchResult from '@/components/utility/searchResult/SearchResult';
+import { mockSearchResultProps } from '@/components/utility/searchResult/SearchResult.mocks';
+import { NextPageWithLayout } from '@/pages/page';
+
+const Results: NextPageWithLayout = () => {
+  const resultData = [...new Array(6)];
+
+  return (
+    <section className="flex flex-col items-center gap-y-5">
+      <div>Search Results</div>
+      <div className="flex flex-col space-y-8">
+        {resultData.map((_, idx) => (
+          <SearchResult key={idx} {...mockSearchResultProps.base} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Results;
+
+Results.getLayout = (page) => {
+  return <PrimaryLayout>{page}</PrimaryLayout>;
+};
