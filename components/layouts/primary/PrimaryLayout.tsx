182a3321f83fcce72a0aad36dc657626b37d062b
dwbrillantes <debrillantes@gmail.com>
2023-01-30 20:05:02 +0800

    feat: update primary layout with header and footer

--- components/layouts/primary/PrimaryLayout.tsx
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -1,5 +1,6 @@
+import Footer from '@/components/navigation/footer/Footer';
+import Header from '@/components/navigation/header/Header';
 import Head from 'next/head';
-import styles from './PrimaryLayout.module.css';
 
 export interface IPrimaryLayout {
   children: React.ReactNode;
@@ -11,7 +12,12 @@ const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
       <Head>
         <title>Primary Layout Example</title>
       </Head>
-      <main className={styles.main}>{children}</main>
+      <main className="min-h-screen flex flex-col items-center ">
+        <Header />
+        {children}
+        <div className="m-auto"></div>
+        <Footer />
+      </main>
     </>
   );
 };
