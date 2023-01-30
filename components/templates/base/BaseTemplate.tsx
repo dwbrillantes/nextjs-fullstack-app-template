aa9b60680da47ad64c11d5a6b574f2ac0e1b2d7b
dwbrillantes <debrillantes@gmail.com>
2023-01-30 16:15:04 +0800

    refactor: removed unused external css modules

--- components/templates/base/BaseTemplate.tsx
+++ components/templates/base/BaseTemplate.tsx
@@ -1,11 +1,13 @@
-import styles from './BaseTemplate.module.css';
-
 export interface IBaseTemplate {
   sampleTextProp: string;
 }
 
 const BaseTemplate: React.FC<IBaseTemplate> = ({ sampleTextProp }) => {
-  return <div className={styles.container}>{sampleTextProp}</div>;
+  return (
+    <div className="bg-gradient-to-bl from-orange-500 via-blue-800 to-black">
+      {sampleTextProp}
+    </div>
+  );
 };
 
 export default BaseTemplate;
