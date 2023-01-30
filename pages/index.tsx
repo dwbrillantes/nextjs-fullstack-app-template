fd47088360108729571ba56efabc1747fa54311f
dwbrillantes <debrillantes@gmail.com>
2023-01-30 20:48:53 +0800

    feat: add google logo to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import PrimaryLayout from '@/components/layouts/primary/PrimaryLayout';
 import Search from '@/components/utility/search/Search';
 import { NextPageWithLayout } from '@/pages/page';
+import Image from 'next/image';
 
 const Home: NextPageWithLayout = () => {
   return (
     <section>
-      <div>Logo</div>
+      <Image
+        src="/images/google-logo.svg"
+        alt="Google Logo"
+        width={272}
+        height={92}
+        priority
+        className="mx-auto mb-3"
+      />
+
       <Search />
       <div>language</div>
     </section>
