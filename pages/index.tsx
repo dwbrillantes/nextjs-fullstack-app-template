92756b2a4b171881878efe63315f483c8d2268bf
dwbrillantes <debrillantes@gmail.com>
2023-01-30 21:05:03 +0800

    feat: add logo to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import PrimaryLayout from '@/components/layouts/primary/PrimaryLayout';
 import Search from '@/components/utility/search/Search';
 import { NextPageWithLayout } from '@/pages/page';
@@ -5,7 +7,7 @@ import Image from 'next/image';
 
 const Home: NextPageWithLayout = () => {
   return (
-    <section>
+    <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
       <Image
         src="/images/google-logo.svg"
         alt="Google Logo"
