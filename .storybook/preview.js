12e1d4e2710fd035bb1ddf29cde4c2906e32b181
dwbrillantes <debrillantes@gmail.com>
2023-02-02 13:45:32 +0800

    feat: add storybook decorator to use AuthProvider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
 import * as NextImage from 'next/image';
+
+import { AuthProvider } from '../context/auth/AuthContext';
 import '../styles/globals.css';
 
 const BREAKPOINTS_INT = {
@@ -32,6 +34,14 @@ Object.defineProperty(NextImage, 'default', {
   configurable: true,
   value: (props) => <OriginalNextImage {...props} unoptimized />,
 });
+
+export const decorators = [
+  (Story) => (
+    <AuthProvider>
+      <Story />
+    </AuthProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
