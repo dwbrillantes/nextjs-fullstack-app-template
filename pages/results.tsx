1ad14cd1068721d884d7b8f39a922ad28664a3e9
dwbrillantes <debrillantes@gmail.com>
2023-02-02 12:37:02 +0800

    feat: update results page to use ssp to search

--- pages/results.tsx
+++ pages/results.tsx
@@ -1,19 +1,49 @@
+import { GetServerSideProps } from 'next';
+
 import PrimaryLayout from '@/components/layouts/primary/PrimaryLayout';
 import SearchResult from '@/components/utility/searchResult/SearchResult';
-import { mockSearchResultProps } from '@/components/utility/searchResult/SearchResult.mocks';
+import { ISearchData } from '@/lib/search/types';
+import { IApiSearchResponseData } from '@/pages/api/search';
 import { NextPageWithLayout } from '@/pages/page';
 
-const Results: NextPageWithLayout = () => {
-  const resultData = [...new Array(6)];
+export interface IResults {
+  searchResults: ISearchData[];
+}
 
+export const getServerSideProps: GetServerSideProps<IResults> = async ({
+  query,
+}) => {
+  let searchResults: IApiSearchResponseData = [];
+  const searchTerm = query.search;
+
+  if (searchTerm && searchTerm.length > 0) {
+    const response = await fetch(`http://localhost:3000/api/search`, {
+      body: JSON.stringify({ searchTerm }),
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+    });
+    searchResults = await response.json();
+  }
+  return {
+    props: {
+      searchResults,
+    },
+  };
+};
+
+const Results: NextPageWithLayout<IResults> = ({ searchResults }) => {
   return (
     <section className="flex flex-col items-center gap-y-5">
-      <div>Search Results</div>
-      <div className="flex flex-col space-y-8">
-        {resultData.map((_, idx) => (
-          <SearchResult key={idx} {...mockSearchResultProps.base} />
-        ))}
-      </div>
+      <div className="font-bold text-xl">Search Results</div>
+      {searchResults.length > 0 ? (
+        <div className="flex flex-col space-y-8">
+          {searchResults.map((result, idx) => (
+            <SearchResult key={idx} {...result} />
+          ))}
+        </div>
+      ) : (
+        <p>No Results Found</p>
+      )}
     </section>
   );
 };
